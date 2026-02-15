db44123cb27b78f0e04eb233d026698eabf73d47
matiibuffaRC <matbuffautn05@gmail.com>
2026-02-15 18:08:11 -0300

    ...

--- src/assets/data/CaruselImages.ts
+++ src/assets/data/CaruselImages.ts
@@ -2,4 +2,8 @@ import img1 from "../imgs/Cremas.png"
 import img2 from "../imgs/Cajas.png"
 import img3 from "../imgs/Bombonitos.png"
 
-export const CaruselImages: string[] = [img1, img2, img3];
+export const CaruselImages: string[] = [
+    img1, 
+    img2,
+    img3
+];
